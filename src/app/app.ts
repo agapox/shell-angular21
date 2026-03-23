79701b0e7f67fe8266ea88a084f58f3880e52cab
Jose Agapito Ramirez <jose_franceschi@unicomer.com>
2026-03-23 00:49:00 -0300

    feat: integrate LegacyHost component into app structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LegacyHost } from "./legacy-host/legacy-host";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LegacyHost],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
